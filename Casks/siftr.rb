352343208bc6476ba7da2eacee80098b625723ab
Suho Park <psooh1219@icloud.com>
2026-06-14 02:14:23 +0900

    Update download URL to public homebrew-siftr releases

--- Casks/siftr.rb
+++ Casks/siftr.rb
@@ -2,7 +2,7 @@ cask "siftr" do
   version "1.1.0"
   sha256 "c492450e010bacbdf47d8b76efe390c6157a397a34d080a354c787ca053e3b36"
 
-  url "https://github.com/SuhoPark-Icloud/Siftr-App/releases/download/v#{version}/Siftr.dmg"
+  url "https://github.com/SuhoPark-Icloud/homebrew-siftr/releases/download/v#{version}/Siftr.dmg"
   name "Siftr"
   desc "Photo culling and similarity analysis tool"
   homepage "https://github.com/SuhoPark-Icloud/Siftr-App"
